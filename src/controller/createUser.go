f7b2d4d10386282bf48d88e908fa54684ea3a20f
GRPORTO95 <gr_porto@hotmail.com>
2025-11-20 18:04:53 -0300

    Implement user request and response models; enhance CreateUser function with JSON binding and error handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
+	"fmt"
+
+	"github.com/gporto95/crud-go/src/controller/model/request"
+
 	"github.com/gporto95/crud-go/src/configuration/rest_err"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Teste")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
